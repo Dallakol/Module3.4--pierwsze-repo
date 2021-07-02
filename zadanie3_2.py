8c3ed11bf7ee15bdea3b92198813409a1e5f20ac
Sebastian Chmielewski <1997chmiel@gmail.com>
2021-07-02 14:34:07 +0200

    Add Init project

--- zadanie3_2.py
+++ zadanie3_2.py
@@ -3,3 +3,9 @@ shopping_dict = {
     "warzywniak," : ["Marchew", "Seler", "Rukola"]
 }
 
+for key, value in shopping_dict.items():
+    print("Idę do", key.capitalize(),  "kupuję tu następujące rzeczy", value)
+
+
+len_values = len(shopping_dict["piekarnia,"]) + len(shopping_dict["warzywniak,"])
+print("W sumie kupuję", len_values , "produktów")
